a930a92a38fd776f2b705727ba3691e178bc3373
JirkaTm <94149048+JirkaTm@users.noreply.github.com>
2025-07-21 20:42:34 +0200

    Fix tests

--- test/test_cicd.py
+++ test/test_cicd.py
@@ -1,5 +1,11 @@
 import pytest
-from fun_factor.app import app
+import os
+import sys
+import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 @pytest.fixture
 def client():
